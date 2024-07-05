a04656e7efcc887d7bb4cfce0b94591166ec59ab
ubeyid oumer <ubeyidah@gmail.com>
2024-07-05 23:40:42 +0300

    create verify or unverify users admin ablity

--- controllers/adminController.controller.js
+++ controllers/adminController.controller.js
@@ -11,4 +11,35 @@ const getAllUsers = async (req, res) => {
   }
 };
 
-export { getAllUsers };
+const deleteUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedUser = await Users.findOneAndDelete({ _id: id });
+    if (!deletedUser)
+      return res.status(404).json({ msg: "User not found!", sucess: false });
+    res.status(200).json({ msg: "User successfully deleted!", sucess: true });
+  } catch (error) {
+    res.status(500).json({ msg: error.message, secess: false });
+  }
+};
+
+const verifyUser = async (req, res) => {
+  try {
+    // verify users if they verified it will fillips or unverify them
+    const { id } = req.params;
+    const matchUser = await Users.findById(id);
+    if (!matchUser)
+      return res.status(404).json({ msg: "User not found", sucess: false });
+    await Users.findOneAndUpdate(
+      { _id: id },
+      { isVerified: !matchUser.isVerified }
+    );
+    res
+      .status(200)
+      .json({ msg: `successfully verified or unverified`, sucess: true });
+  } catch (error) {
+    res.status(500).json({ msg: error.message, secess: false });
+  }
+};
+
+export { getAllUsers, deleteUser, verifyUser };
--- routes/adminController.route.js
+++ routes/adminController.route.js
@@ -1,7 +1,13 @@
 import { Router } from "express";
 import adminRoute from "../middlewares/adminRoute.js";
-import { getAllUsers } from "../controllers/adminController.controller.js";
+import {
+  getAllUsers,
+  deleteUser,
+  verifyUser,
+} from "../controllers/adminController.controller.js";
 const router = Router();
 
 router.get("/", adminRoute, getAllUsers);
+router.delete("/:id", adminRoute, deleteUser);
+router.put("/:id", adminRoute, verifyUser);
 export default router;
