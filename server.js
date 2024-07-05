84d29d12e49f25217e0ddc194c0b63695a6ce986
ubeyid oumer <ubeyidah@gmail.com>
2024-07-05 17:24:21 +0300

    create store item route

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ import { config } from "dotenv";
 import connectToDb from "./config/connect.db.js";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import storeItemRoutes from "./routes/storeItem.route.js";
 
 const app = express();
 config();
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use("/api/storeItem", storeItemRoutes);
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
