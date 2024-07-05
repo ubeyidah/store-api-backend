16a1c9de02de5f8e4dd55c97113715d2b9d19b54
ubeyid oumer <ubeyidah@gmail.com>
2024-07-05 11:45:48 +0300

    create login logic

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -6,9 +6,7 @@ const signup = async (req, res) => {
   try {
     const { userName, email, password } = req.body;
     if (!userName || !email || !password) {
-      return res
-        .status(400)
-        .json({ msg: "all fileds are required", secess: false });
+      return res.status(400).json({ msg: "fill out the filed", secess: false });
     }
     const isUserExist = await Users.findOne({ email });
     if (isUserExist) {
@@ -39,7 +37,33 @@ const signup = async (req, res) => {
     res.status(500).json({ msg: error.message, secess: false });
   }
 };
-const login = async (req, res) => {};
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password)
+      return res.status(400).json({ msg: "fill out the filed", secess: false });
+    const user = await Users.findOne({ email });
+    if (!user)
+      return res
+        .status(404)
+        .json({ msg: "no email adress found", secess: false });
+    const isMatchPassword = await bcrypt.compare(password, user.password);
+    if (!isMatchPassword)
+      return res.status(400).json({ msg: "password mismatch", secess: false });
+    genAndSetToken(user._id, res);
+    res.status(200).json({
+      sucess: true,
+      data: {
+        id: user._id,
+        userName: user.userName,
+        email: user.email,
+        isAdmin: user.isAdmin,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ msg: error.message, secess: false });
+  }
+};
 const logout = async (req, res) => {};
 
 export { signup, login, logout };
