bfece531ce1781ab039e91cf8ed5934698dce308
ubeyid oumer <ubeyidah@gmail.com>
2024-07-05 10:29:30 +0300

    create user model

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,4 +1,16 @@
-const signup = async (req, res) => {};
+const signup = async (req, res) => {
+  try {
+    const { userName, email, password, isAdmin } = req.body;
+    // valid inputs
+    // check if it exit in db
+    // hash password
+    // save user
+    // set token and save it in cookie
+    // response new user
+  } catch (error) {
+    res.status(500).json({ msg: "internal server error", seccess: false });
+  }
+};
 const login = async (req, res) => {};
 const logout = async (req, res) => {};
 
---
+++ models/users.model.js
@@ -0,0 +1,28 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      required: [true, "user name must provied"],
+      unique: [true, "this user name alrady exists"],
+      minLength: [3, "user name must be more than 3 characters"],
+    },
+    email: {
+      type: String,
+      required: [true, "email must provied"],
+      unique: [true, "this email alrady exists"],
+    },
+    password: {
+      type: String,
+      required: [true, "password must provied"],
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Users = model("user", userSchema);
