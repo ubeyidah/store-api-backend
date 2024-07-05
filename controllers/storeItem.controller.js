a571da2e54c9ef6ee1d6d394733ebeadbee25139
ubeyid oumer <ubeyidah@gmail.com>
2024-07-05 19:33:56 +0300

    create store item update logic

--- controllers/storeItem.controller.js
+++ controllers/storeItem.controller.js
@@ -29,4 +29,26 @@ const getAllStoreItems = async (req, res) => {
   }
 };
 
-export { createStoreItem, getAllStoreItems };
+const updateStoreItem = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, catagory, measure } = req.body;
+    if (!name || !catagory || !measure)
+      return res.status(400).json({
+        msg: "fill out missing name or catagory or measure.",
+        sucess: false,
+      });
+    const storeItem = await storeItemModel.findById(id);
+    if (!storeItem)
+      return res.status(404).json({ msg: "item not found", sucess: false });
+    await storeItemModel.findOneAndUpdate(
+      { _id: id },
+      { name, catagory, measure }
+    );
+    res.status(200).json({ msg: "successfully updated.", sucess: true });
+  } catch (error) {
+    res.status(500).json({ error: error.message, sucess: false });
+  }
+};
+
+export { createStoreItem, getAllStoreItems, updateStoreItem };
--- routes/storeItem.route.js
+++ routes/storeItem.route.js
@@ -2,11 +2,14 @@ import { Router } from "express";
 import {
   createStoreItem,
   getAllStoreItems,
+  updateStoreItem,
 } from "../controllers/storeItem.controller.js";
 import protectRoute from "../middlewares/protectRoutes.js";
+import adminRoute from "../middlewares/adminRoute.js";
 
 const router = Router();
 
-router.post("/create", protectRoute, createStoreItem);
 router.get("/", protectRoute, getAllStoreItems);
+router.post("/create", protectRoute, createStoreItem);
+router.put("/:id", adminRoute, updateStoreItem);
 export default router;
